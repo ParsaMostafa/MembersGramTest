44b09fdf742d8c4be840ade8fba5cd5f11829c16
NeoA <brucemostafa10@gmail.com>
2023-06-24 12:37:53 -0700

    MainIntro page xml Done

--- app/src/main/java/com/example/membersgramtest/MainActivity.kt
+++ app/src/main/java/com/example/membersgramtest/MainActivity.kt
@@ -6,8 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-
+        val mainActivityLayout = MainActivityLayout(this)
+        setContentView(mainActivityLayout)
     }
+
 }
