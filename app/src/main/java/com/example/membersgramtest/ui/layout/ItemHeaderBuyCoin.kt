980f5f71c1b69df00ce9dd1e21bb9330484dc712
NeoA <brucemostafa10@gmail.com>
2023-08-21 02:26:49 -0700

    Retengle for tab buycoin is done

--- app/src/main/java/com/example/membersgramtest/ui/layout/ItemHeaderBuyCoin.kt
+++ app/src/main/java/com/example/membersgramtest/ui/layout/ItemHeaderBuyCoin.kt
@@ -1,13 +1,15 @@
 package com.example.membersgramtest.ui.layout
 
 import android.content.Context
+import android.graphics.BlurMaskFilter
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
-import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import com.example.membersgramtest.R
+import com.example.membersgramtest.utillity.Metrics
 
 class ItemHeaderBuyCoin(
     context: Context,
@@ -15,15 +17,28 @@ class ItemHeaderBuyCoin(
     defStyleAttr: Int = 0
 ) : ViewGroup(context, attrs, defStyleAttr) {
 
-    private val borderPaint = Paint()
-    private val desiredBorderWidth = context.resources.displayMetrics.density * 8 // 8dp border
+    private val backgroundColor = ContextCompat.getColor(context, R.color.light_blue) // #E3F2FD
+
+    private val backgroundPaint = Paint()
+    private val shadowPaint = Paint()
+
     private val desiredHeight = context.resources.displayMetrics.density * 89 // 89dp height
+    private val horizontalPadding = context.resources.displayMetrics.density * 16 // 16dp padding
+    private val topPadding = context.resources.displayMetrics.density * 16 // 16dp padding from the top
+    private val cornerRadius = context.resources.displayMetrics.density * 5 // 5dp corner radius
+    private val shadowRadius = context.resources.displayMetrics.density * 10 // 10dp shadow radius
+    private val shadowOffsetX = context.resources.displayMetrics.density * 0 // No horizontal shadow offset
+    private val shadowOffsetY = context.resources.displayMetrics.density * 4 // 4dp vertical shadow offset
 
     init {
-        val borderColor = ContextCompat.getColor(context, R.color.blueindt)
-        borderPaint.color = borderColor
-        borderPaint.style = Paint.Style.STROKE
-        borderPaint.strokeWidth = desiredBorderWidth
+        backgroundPaint.color = backgroundColor
+        backgroundPaint.style = Paint.Style.FILL
+
+        // Create a BlurMaskFilter for the shadow paint
+        val blurMaskFilter = BlurMaskFilter(shadowRadius, BlurMaskFilter.Blur.NORMAL)
+        shadowPaint.maskFilter = blurMaskFilter
+        shadowPaint.color = ContextCompat.getColor(context, android.R.color.darker_gray)
+        shadowPaint.style = Paint.Style.FILL
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
@@ -35,19 +50,22 @@ class ItemHeaderBuyCoin(
         // Measure child views and calculate the size of this view
         val width = MeasureSpec.getSize(widthMeasureSpec)
         val height = desiredHeight.toInt()
-        setMeasuredDimension(width, height)
+        setMeasuredDimension(width, Metrics.dpToPx(105))
     }
 
     override fun dispatchDraw(canvas: Canvas) {
-        // Draw the border using the canvas
-        val halfBorderWidth = borderPaint.strokeWidth / 2
-        canvas.drawRect(
-            halfBorderWidth,
-            halfBorderWidth,
-            width - halfBorderWidth,
-            desiredHeight.toFloat(),
-            borderPaint
-        )
+        // Draw the background color with shadow using the canvas
+        val left = horizontalPadding
+        val right = width - horizontalPadding
+        val top = topPadding
+        val bottom = desiredHeight.toFloat()
+
+        // Draw shadow first
+        canvas.drawRoundRect(left + shadowOffsetX, top + shadowOffsetY, right + shadowOffsetX, bottom + shadowOffsetY, cornerRadius, cornerRadius, shadowPaint)
+
+        // Draw the background color
+        canvas.drawRoundRect(left, top, right, bottom, cornerRadius, cornerRadius, backgroundPaint)
+
         super.dispatchDraw(canvas)
     }
 }
