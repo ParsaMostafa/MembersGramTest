ae0b73c33e35b499f22d57f40f5e270d2216f234
bruce <brucemostafa10@gmail.com>
2023-09-23 12:27:07 +0330

    MymemberViewmodel is done

--- app/src/main/java/com/example/membersgramtest/models/MyMemberModel/MyMemberModel.kt
+++ app/src/main/java/com/example/membersgramtest/models/MyMemberModel/MyMemberModel.kt
@@ -4,10 +4,11 @@ import android.widget.ImageView
 
 sealed class MyMemberModel {
 
-    data  class MyMemberHeaderModel(val accountImage : ImageView , val title: String , val number : String) : MyMemberModel()
+    data  class MyMemberHeaderModel(val accountImage : Int , val title: String , val number : String?) : MyMemberModel()
 
 
-    data  class MyMemberBodyModel(val icon : ImageView , val icontitle: String ) : MyMemberModel()
+    data class MyMemberBodyModel(val icon1: Int, val title1: String, val icon2: Int, val title2: String, val icon3: Int, val title3: String, val icon4: Int, val title4: String) : MyMemberModel()
+
 
     object MyMemberFooterModel :  MyMemberModel()
 }
--- app/src/main/java/com/example/membersgramtest/viewmodel/MyMemberViewmodel.kt
+++ app/src/main/java/com/example/membersgramtest/viewmodel/MyMemberViewmodel.kt
@@ -1,11 +1,43 @@
-package com.example.membersgramtest.viewmodel
-
-import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.membersgramtest.R
+import com.example.membersgramtest.models.MyMemberModel.MyMemberModel
+import com.example.membersgramtest.sharedPerf.PreferencesHelper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-class MyMemberViewmodel:ViewModel(){
+class MyMemberViewmodel : ViewModel() {
+    val TabMyMemberList = MutableStateFlow<List<MyMemberModel>>(emptyList())
 
-    init {
-        Log.d("MemberDetViewmodel0","viewmodelcreated")
+    fun createListOfMyMemberTab() {
+        val preferencesHelper = PreferencesHelper
+        val phoneNumber = preferencesHelper.phoneNumber
+
+        val MyMemberModelItems = mutableListOf<MyMemberModel>()
+
+        MyMemberModelItems.add(
+            MyMemberModel.MyMemberHeaderModel(
+                R.drawable.person_black_24dp__1_,
+                "Borzoe Bastae",
+                phoneNumber
+            )
+        )
+
+        MyMemberModelItems.add(
+            MyMemberModel.MyMemberBodyModel(
+                R.drawable.payment_black_24dp, "Payments",
+                R.drawable.ic_support, "Support",
+                R.drawable.settings_24px, "Settings",
+                R.drawable.logout_black_24dp, "Log out"
+            )
+        )
+
+        MyMemberModelItems.add(MyMemberModel.MyMemberFooterModel)
+
+        // Create a coroutine scope and emit the MyMemberModelItems within it
+        CoroutineScope(Dispatchers.IO).launch {
+            TabMyMemberList.emit(MyMemberModelItems)
+        }
     }
 }
