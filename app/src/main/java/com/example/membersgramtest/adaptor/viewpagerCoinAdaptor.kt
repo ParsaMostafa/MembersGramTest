7f599495676d8f596105ed451d6cd505504a69ad
bruce <brucemostafa10@gmail.com>
2023-09-23 09:12:26 +0330

    tab Transfer LOGIC And UI is COPMPLATE and ETC

--- app/src/main/java/com/example/membersgramtest/adaptor/viewpagerCoinAdaptor.kt
+++ app/src/main/java/com/example/membersgramtest/adaptor/viewpagerCoinAdaptor.kt
@@ -1,19 +1,29 @@
 package com.example.membersgramtest.adaptor
 
-
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.membersgramtest.ui.fragment.FragCoinBuy
-import com.example.membersgramtest.ui.fragment.FragCoinTransfer
+import com.example.membersgramtest.ui.fragment.FragmentTransferCoin
+import android.util.Log // Import the Log class
+import androidx.fragment.app.FragmentManager
+import com.bumptech.glide.manager.Lifecycle
+import com.example.membersgramtest.viewmodel.TransferViewModel
 
+class viewpagerCoinAdaptor(fm: FragmentManager, lifecycle: androidx.lifecycle.Lifecycle, list:ArrayList<Fragment>) : FragmentStateAdapter(fm,lifecycle) {
 
-class viewpagerCoinAdaptor(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 2
+    private val fragmentList : ArrayList <Fragment> = list
+    override fun getItemCount(): Int {
+        val itemCount = 2
+        Log.d("viewpagerCoinAdaptor", "getItemCount() called with itemCount: $itemCount")
+        return itemCount
+    }
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
+        val fragment = when (position) {
             0 -> FragCoinBuy()
-            else -> FragCoinTransfer()
+            else -> FragmentTransferCoin()
         }
+        Log.d("viewpagerCoinAdaptor", "createFragment() called with position: $position")
+        return fragment
     }
 }
